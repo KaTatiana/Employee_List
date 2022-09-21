cce8f1de71a7ece7a4bdb39ba93eeac1410ce288
KaTatiana <badinat@mail.ru>
2022-09-22 00:48:12 +0300

    Initial commit

---
+++ src/test/java/proskyEmployeeList/EmployeeListApplicationTests.java
@@ -0,0 +1,13 @@
+package proskyEmployeeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
